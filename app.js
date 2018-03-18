3f1c9972b68a2f40e4c98936c434e7c7722e9183
Ola Karlsson <ola.anders.karlsson@gmail.com>
2018-03-18 23:05:24 +0100

    adderat app.js och ändrat till .ejs

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app     = express();
+app.use(express.static(__dirname + '/views'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/script'));
+//Store all JS and CSS in Scripts folder.
+
+app.get('/',function(req,res){
+	// res.sendFile('index.html');
+	res.render('index.ejs');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+	var appConsoleMsg = 'YelpCamp server has started: ';
+	appConsoleMsg += process.env.IP + ':' + process.env.PORT;
+	console.log(appConsoleMsg);
+});
